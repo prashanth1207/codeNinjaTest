ae6abca7e1835558b11a9b4b57826e7d223d3950
Prashanth N <prashanth.t.r.n@gmail.com>
2020-03-06 13:11:00 -0800

    changed ip address

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 app.set('view engine', 'ejs');
 
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: 'http://localhost:4200', credentials: true }));
+app.use(cors({ origin: 'http://18.224.52.140:4200', credentials: true }));
 
 
 // app.use(bodyParser.urlencoded({
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 //Allow Access Control
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://18.224.52.140:4200');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
